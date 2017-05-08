91de39376c7e992f62edcbd94b89baffa7fbf02a
Konrad Lalik <konrad.lalik@makingwaves.com>
2017-05-08 15:07:52 +0200

    Fix library

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,11 @@
 import * as jQuery from "jquery";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { AppContainer } from 'react-hot-loader';
 
 import { Hello } from "./components/Hello";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-    <AppContainer>
-        <Hello compiler="Typescript" framework="React" />
-    </AppContainer>,
+    <Hello compiler="Making" framework="React" />,
     document.getElementById("app")
 );
